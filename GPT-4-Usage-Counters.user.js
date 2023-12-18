38dfa4bca5151ea029b72e2837efeb18b4732166
lugia19 <lugia19@lugia19.com>
2023-12-18 19:56:20 +0100

    Version bump for greasyfork

--- GPT-4-Usage-Counters.user.js
+++ GPT-4-Usage-Counters.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT GPT-4 Counters
 // @namespace 	 lugia19.com
-// @version      1.0
+// @version      1.1
 // @description  Add counters (and reset time indicators) for GPT-4/Custom GPTs to ChatGPT
 // @author       lugia19
 // @license		 MIT
